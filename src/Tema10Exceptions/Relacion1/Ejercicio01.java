12808ba3b1e75b844971a975a0232af96fbaedc3
IvanSM36 <ivan_sm@hotmail.es>
2022-04-01 10:18:56 +0200

    ejer relacion 1 exception

--- src/Tema10Exceptions/Relacion1/Ejercicio01.java
+++ src/Tema10Exceptions/Relacion1/Ejercicio01.java
@@ -7,17 +7,31 @@ public class Ejercicio01 {
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
-		int aux = 0;
+		boolean datoValido = false;
+		double aux = 0;
+		int contador = 0;
 		
-		for (int i = 0; i < 6; i++) {
-			System.out.print("Introduzca un numero: ");
-			int num = scan.nextInt();		
+		do {
+			try {
+				do {
+					System.out.print("Introduzca un numero: ");
+					double num = Double.parseDouble(scan.nextLine());		
+					datoValido = true;
+					contador++;
+					if (aux < num) {
+						aux = num;
+					}
+				} while (contador < 6);
+				
+				datoValido = false;
+				
+				System.out.println("El numero maximo es: " + aux);
+
+			} catch (Exception e) {
+				System.out.println("Debes introducir un numero.");
+			}
+		} while(!datoValido);
 		
-			if(aux < num)
-				aux = num;
-		}
-		
-		System.out.println("El numero maximo es: " + aux);
 
 	}
 
